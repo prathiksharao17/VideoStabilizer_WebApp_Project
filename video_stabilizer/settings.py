ed1ee7baa9d89bfae4bc61a9d7b5ee75327ccc7b
Prathiksha Rao <prathiksharao17@gmail.com>
2025-06-16 14:49:32 +0530

    updated settings

--- video_stabilizer/settings.py
+++ video_stabilizer/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-673hepbi#qxa!&rymxue(+$0-fj%=$w6_^xl8rbu=&zlp3e5ir'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS =[]
+ALLOWED_HOSTS =['https://videostabilizer-webapp-project.onrender.com/']
 
 
 # Application definition
